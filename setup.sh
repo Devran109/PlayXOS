d052e44ba660754fdefc0af6bbfa422c322f8099
Devran109 <88400645+Devran109@users.noreply.github.com>
2022-04-27 08:02:51 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt install neofetch -y
+sudo apt install lightdm -y
+sudo apt install openbox -y
+sudo apt install openbox-gnome-session -y
+sudo apt install wget -y
+sudo apt install gnome-terminal -y
+sudo reboot now
