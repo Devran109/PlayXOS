e7b17f864a18c9db38d8016e4534b2d3b00c069c
Devran109 <88400645+Devran109@users.noreply.github.com>
2022-04-28 14:26:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,12 @@ sudo apt install tint2 -y
 sudo apt install gedit -y
 sudo apt install nitrogen-y
 sudo apt install ubuntu-wallpapers -y
-sudo apt install  -y
+sudo apt install xcompmgr -y
+sudo apt install volti -y
+sudo apt install gconf-editor -y
+sudo apt install pavucontrol -y
+sudo apt install lxappearance -y
+sudo apt install pcmanfm -y
 sudo apt install docky -y
 sudo apt install wget -y
 sudo apt install gnome-terminal -y
