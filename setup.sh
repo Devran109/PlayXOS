6c8c0abfa478e9a2101565fc7aa3bd082de27bb2
Devran109 <88400645+Devran109@users.noreply.github.com>
2022-04-28 13:54:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ sudo apt install openbox -y
 sudo apt install openbox-gnome-session -y
 sudo apt install chromium-browser -y
 sudo apt install tint2 -y
+sudo apt install gedit -y
+sudo apt install nitrogen-y
+sudo apt install ubuntu-wallpapers -y
+sudo apt install  -y
 sudo apt install docky -y
 sudo apt install wget -y
 sudo apt install gnome-terminal -y
