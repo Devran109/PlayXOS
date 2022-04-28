b6e35fbe5d95955ab0d597487c3f08a228671762
Devran109 <88400645+Devran109@users.noreply.github.com>
2022-04-28 09:57:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@ sudo apt install neofetch -y
 sudo apt install lightdm -y
 sudo apt install openbox -y
 sudo apt install openbox-gnome-session -y
+sudo apt install chromium-browser -y
+sudo apt install tint2 -y
+sudo apt install docky -y
 sudo apt install wget -y
 sudo apt install gnome-terminal -y
 sudo reboot now
